f6b3763a6fc3dc37928ce861558654bd03b3988c
Risal <risalhidayat88@gmail.com>
2024-11-25 14:22:39 +0700

    add relationship, validation and hooks to webhook and webhook request model

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,2 +1,17 @@
 class Webhook < ApplicationRecord
+  before_validation :set_slug
+
+  validates :expired_at, presence: true
+
+  has_many :webhook_requests, dependent: :destroy
+
+  def expired?
+    expired_at < Date.today
+  end
+
+  private
+
+  def set_slug
+    self.slug = SecureRandom.uuid
+  end
 end
--- app/models/webhook_request.rb
+++ app/models/webhook_request.rb
@@ -1,3 +1,13 @@
 class WebhookRequest < ApplicationRecord
+  # Convert headers and query params to JSON before saving
+  before_save :ensure_json_format
+
   belongs_to :webhook
+
+  private
+
+  def ensure_json_format
+    self.headers = headers.to_json unless headers.is_a?(String)
+    self.query_params = query_params.to_json unless query_params.is_a?(String)
+  end
 end
