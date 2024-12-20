cd1bd609a1c396bad3bf8a3c9a34da9cc3562808
Risal Hidayat <risal.hidayat@kawanlamacorp.com>
2024-12-21 00:32:08 +0700

    implement realtime update using action cable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    render plain: "Hello"
+    webhook = Webhook.create(expired_at: 1.week.from_now)
+
+    redirect_to webhook_path(webhook.slug)
   end
 end
